5319fc1a6da14dd798196a876d1dd25c372d189f
Carsten Richter <carsten.richter@physik.tu-freiberg.de>
2013-04-09 13:32:29 +0200

    Unified setup.py and setup_singlecore.py and updated install.txt

--- pyxrr/setup.py
+++ pyxrr/setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
 import numpy
+import sys
+
+if len(sys.argv)<2:
+    print("see install.txt for installation instructions.")
+if "--single-core" in sys.argv[1:]:
+    ext_modules = [Extension("pyxrr.xrr", ["pyxrr/xrr_sc.c"], include_dirs = [numpy.get_include()])]
+    sys.argv.pop(sys.argv.index("--single-core"))
+else:
+    ext_modules = [Extension("pyxrr.xrr", ["pyxrr/xrr.c"], include_dirs = [numpy.get_include()], extra_compile_args = ["-fopenmp"], extra_link_args = ["-fopenmp"])]
 
 setup( name = "xrr", 
        version = "0.9.07",
-       ext_modules = [Extension("pyxrr.xrr", ["pyxrr/xrr.c"], include_dirs = [numpy.get_include()], extra_compile_args = ["-fopenmp"], extra_link_args = ["-fopenmp"])],
+       ext_modules = ext_modules,
        packages = ["pyxrr"],
        package_data={'pyxrr': ['materials.sqlite',
                                'locale/en/LC_MESSAGES/*',
@@ -17,3 +26,4 @@ setup( name = "xrr",
                            It is supposed to be used by the python xrr wrapper (pyxrr)\n\n\
                            See xrr.reflectivity for more details.",
      )
+
