02be772e7a6c5d17006a7a0192ad002912838330
Carsten Richter <carichte@users.noreply.github.com>
2026-09-19 11:45:36 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python
-from distutils.core import setup, Extension
-import numpy
-import sys
+import os, sys
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-
-
-#raise Exception("You downloaded the (not working/under construction) developement version. "
-#                "Please refer to http://carichte.github.io/pyxrr/ to download "
-#                "the stable Version.")
 
 
 if len(sys.argv)<2:
     print("see install.txt for installation instructions.")
 
+
+class build_shared_lib(build_ext):
+    """Compile libxrr.c as a plain shared library (loaded via ctypes)."""
+    def build_extension(self, ext):
+        target = os.path.join(self.build_lib, 'pyxrr', 'libxrr')
+        os.makedirs(os.path.dirname(target), exist_ok=True)
+
+        if self.compiler.compiler_type == 'msvc':
+            compile_args = ['/openmp']
+            link_args = []
+        else:
+            compile_args = ext.extra_compile_args
+            link_args = list(ext.extra_link_args)
+            if sys.platform == 'win32':
+                link_args += ['-static-libgcc',
+                              '-Wl,-Bstatic',
+                              '-lgomp', '-lwinpthread', '-ldl',
+                              '-Wl,-Bdynamic']
+        
+        objects = self.compiler.compile(
+            ext.sources,
+            output_dir=self.build_temp,
+            extra_postargs=compile_args,
+            include_dirs=ext.include_dirs)
+
+        if sys.platform == 'win32':
+            target += '.dll'
+        else:
+            target += '.so'
+
+        self.compiler.link_shared_object(
+            objects, target,
+            extra_postargs=link_args)
+
+
 ext_modules = [Extension("pyxrr.libxrr",
                ["pyxrr/libxrr.c"],
-               include_dirs = [numpy.get_include()], 
-               extra_compile_args = ["-fopenmp"],
-               extra_link_args = ["-fopenmp"])]
+               include_dirs=[],
+               extra_compile_args=["-fopenmp"],
+               extra_link_args=["-fopenmp"])]
 
 
 setup(
     name = "pyxrr", 
     version = "1.0.0",
     ext_modules = ext_modules,
+    cmdclass={'build_ext': build_shared_lib},
     packages = ["pyxrr"],
     package_data={'pyxrr': [
         'f1f2.h5',
@@ -42,7 +73,7 @@ setup(
         'matplotlib',
         'scipy',
         'h5py',
-        'pandas ',
+        'pandas',
     ],
 )
 
